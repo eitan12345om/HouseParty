d03877b4f56222d57db9d7f0dd250e12bbb26d39
Jackson Kurtz <jkurtz678@gmail.com>
2018-02-25 15:42:19 -0800

    song and playlist java classes

---
+++ app/src/main/java/com/houseparty/houseparty/Playlist.java
@@ -0,0 +1,31 @@
+package com.houseparty.houseparty;
+
+/**
+ * Created by jacksonkurtz on 2/23/18.
+ */
+import java.util.ArrayList;
+
+public class Playlist {
+    private String name;
+    private ArrayList<Song> songs;
+
+    public Playlist() {
+    }
+
+    public Playlist( String name ){
+        this.name = name;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public void addSong( Song song ){
+        this.songs.add(song);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public ArrayList<Song> getSongs(){
+        return this.songs;
+    }
+}
---
+++ app/src/main/java/com/houseparty/houseparty/Song.java
@@ -0,0 +1,23 @@
+package com.houseparty.houseparty;
+
+/**
+ * Created by jacksonkurtz on 2/23/18.
+ */
+
+public class Song {
+    private String title;
+    private String artist;
+    private String uri;
+    private String api;
+
+    public Song() {
+    }
+
+    public Song( String title, String artist, String uri, String api) {
+        this.title = title;
+        this.artist = artist;
+        this.uri = uri;
+        this.api = api;
+    }
+
+}
