f5776ccc1d0b37465db065c446ff0500da8d9e01
Eitan Simler <eitan.simler@gmail.com>
2018-04-09 16:13:11 -0700

    Added PlaylistUnitTests

---
+++ app/src/test/java/com/houseparty/houseparty/PlaylistUnitTest.java
@@ -0,0 +1,34 @@
+package com.houseparty.houseparty;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PlaylistUnitTest {
+
+    private Playlist playlist;
+
+    @Test
+    public void test_toString_oneSong() throws Exception {
+        playlist = new Playlist("Jackson", "12345");
+        playlist.addSong(new Song("Thriller", "fake_uri"));
+        assertTrue(playlist.toString().equals("JacksonThriller"));
+    }
+
+    @Test
+    public void test_toString_twoSongs() throws Exception {
+        playlist = new Playlist("Jackson", "12345");
+        playlist.addSong(new Song("Thriller", "fake_uri"));
+        playlist.addSong(new Song("Beat It", "fake_uri2"));
+        assertTrue(playlist.toString().equals("JacksonThriller"));
+    }
+
+    @Test
+    public void test_toString_twoSongs2() throws Exception {
+        playlist = new Playlist("Jackson", "12345");
+        playlist.addSong(new Song("Beat It", "fake_uri2"));
+        playlist.addSong(new Song("Thriller", "fake_uri"));
+        assertTrue(playlist.toString().equals("JacksonBeat It"));
+    }
+}
