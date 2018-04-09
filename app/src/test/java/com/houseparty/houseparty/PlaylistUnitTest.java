0359d57aca7107e9fcfcea43b3d074c71b5d6dd5
Eitan Simler <eitan.simler@gmail.com>
2018-04-09 16:18:47 -0700

    Add Song tests

--- app/src/test/java/com/houseparty/houseparty/PlaylistUnitTest.java
+++ app/src/test/java/com/houseparty/houseparty/PlaylistUnitTest.java
@@ -2,7 +2,6 @@ package com.houseparty.houseparty;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PlaylistUnitTest {
