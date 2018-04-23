31fa1b05b5b1ae8197063efe2a8ae732769b65a5
Eitan Simler <eitan.simler@gmail.com>
2018-04-23 15:53:59 -0700

    Added Cucumber stuff to try to get it to work

--- app/src/test/java/com/houseparty/houseparty/cucumber/CucumberRunner.java
+++ app/src/test/java/com/houseparty/houseparty/cucumber/CucumberRunner.java
@@ -1,5 +1,7 @@
 package com.houseparty.houseparty.cucumber;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
@@ -7,11 +9,32 @@ import cucumber.api.junit.Cucumber;
 
 /**
  * @author Eitan created on 4/20/2018.
+ * <p>
+ * For a tutorial about Cucumber, go to:
+ * http://www.srccodes.com/p/article/48/cucumber-test-behavior-driven-development-bdd-feature-step-runner-glue-gherkin-data-table-scenario-given-when-then
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        format = {"pretty", "json:target/"},
-        features = {"src/test/java/com/houseparty/houseparty/features"}
+    format = {
+        "pretty",
+        "json:src/test/java/com/houseparty/houseparty/cucumber/Cucumber.json",
+        "html:src/test/java/com/houseparty/houseparty/cucumber/Cucumber.html"
+    },
+    glue = {
+        "src/test/java/com/houseparty/houseparty/cucumber_steps"
+    },
+    features = {
+        "src/test/java/com/houseparty/houseparty/features"
+    }
 )
 public class CucumberRunner {
+    @BeforeClass
+    public void setup() {
+        return;
+    }
+
+    @AfterClass
+    public void teardown() {
+        return;
+    }
 }
