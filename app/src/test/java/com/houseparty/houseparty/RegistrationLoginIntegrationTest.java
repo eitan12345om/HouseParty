4652de1a93ab2594bdc4d7ef759f34664c69e7a7
Eitan Simler <eitan.simler@gmail.com>
2018-05-07 15:37:19 -0700

    Commented out old unit test methods

--- app/src/test/java/com/houseparty/houseparty/RegistrationLoginIntegrationTest.java
+++ app/src/test/java/com/houseparty/houseparty/RegistrationLoginIntegrationTest.java
@@ -1,10 +1,6 @@
 package com.houseparty.houseparty;
 
 import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by jacksonkurtz on 4/4/18.
@@ -15,38 +11,38 @@ public class RegistrationLoginIntegrationTest {
     private LoginActivity activity;
 
     @Before
-    public void setup() throws Exception {
+    public void setup() {
         activity = new LoginActivity();
     }
 
-    @Test
-    public void createAndLoginUser_isCorrect() throws Exception {
-        assertTrue(activity.createUserAndLogin("jkurtz678@gmail.com", "12345"));
-    }
-
-    @Test
-    public void createAndLoginUser_incorrectPassword() throws Exception {
-        assertFalse(activity.createUserAndLogin("jkurtz678@gmail.com", "12346"));
-    }
-
-    @Test
-    public void createAndLoginUser_nullLogin() throws Exception {
-        assertFalse(activity.createUserAndLogin(null, "12345"));
-    }
-
-    @Test
-    public void createAndLoginUser_nullPassword() throws Exception {
-        assertFalse(activity.createUserAndLogin("jkurtz678@gmail.com", null));
-    }
-
-    @Test
-    public void createAndLoginUser_invalidEmail() throws Exception {
-        assertFalse(activity.createUserAndLogin("jkurtz678", null));
-    }
-
-    @Test
-    public void createAndLoginUser_badPasswordLength() throws Exception {
-        assertFalse(activity.createUserAndLogin("jkurtz678@gmail.com", "123"));
-    }
+//    @Test
+//    public void createAndLoginUser_isCorrect() throws Exception {
+//        assertTrue(activity.createUserAndLogin("jkurtz678@gmail.com", "12345"));
+//    }
+//
+//    @Test
+//    public void createAndLoginUser_incorrectPassword() throws Exception {
+//        assertFalse(activity.createUserAndLogin("jkurtz678@gmail.com", "12346"));
+//    }
+//
+//    @Test
+//    public void createAndLoginUser_nullLogin() throws Exception {
+//        assertFalse(activity.createUserAndLogin(null, "12345"));
+//    }
+//
+//    @Test
+//    public void createAndLoginUser_nullPassword() throws Exception {
+//        assertFalse(activity.createUserAndLogin("jkurtz678@gmail.com", null));
+//    }
+//
+//    @Test
+//    public void createAndLoginUser_invalidEmail() throws Exception {
+//        assertFalse(activity.createUserAndLogin("jkurtz678", null));
+//    }
+//
+//    @Test
+//    public void createAndLoginUser_badPasswordLength() throws Exception {
+//        assertFalse(activity.createUserAndLogin("jkurtz678@gmail.com", "123"));
+//    }
 
 }
--- app/src/test/java/com/houseparty/houseparty/RegistrationUnitTest.java
+++ app/src/test/java/com/houseparty/houseparty/RegistrationUnitTest.java
@@ -1,10 +1,5 @@
 package com.houseparty.houseparty;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 /**
  * Created by jacksonkurtz on 4/4/18.
  */
@@ -13,42 +8,42 @@ public class RegistrationUnitTest {
 
     private LoginActivity activity = new LoginActivity();
 
-    @Test
-    public void register_correctInputs() throws Exception {
-        assertTrue(activity.register("jkurtz678@gmail.com", "12345"));
-    }
-
-    @Test
-    public void register_existingUsername() throws Exception {
-        assertFalse(activity.register("matt@gmail.com", "12345"));
-    }
-
-    @Test
-    public void register_nullUsername() throws Exception {
-        assertFalse(activity.register(null, "12345"));
-    }
-
-    @Test
-    public void register_nullPassword() throws Exception {
-        assertFalse(activity.register("jkurtz678@gmail.com", null));
-    }
-
-    @Test
-    public void register_badEmail() throws Exception {
-        assertFalse(activity.register("jkurtz678", "12345"));
-    }
-
-    // longer than 4, shorter than 16
-    @Test
-    public void register_badPasswordLength() throws Exception {
-        assertTrue(activity.register("jkurtz678@gmail.com", "12345"));
-        assertFalse(activity.register("jkurtz678@gmail.com", "123"));
-        assertFalse(activity.register("jkurtz678@gmail.com", "123423123134235111"));
-    }
-
-    @Test
-    public void register_invalidCharacters() throws Exception {
-        assertFalse(activity.register("jac##  kson kurtz@gmail.com", "12345"));
-
-    }
+//    @Test
+//    public void register_correctInputs() throws Exception {
+//        assertTrue(activity.register("jkurtz678@gmail.com", "12345"));
+//    }
+//
+//    @Test
+//    public void register_existingUsername() throws Exception {
+//        assertFalse(activity.register("matt@gmail.com", "12345"));
+//    }
+//
+//    @Test
+//    public void register_nullUsername() throws Exception {
+//        assertFalse(activity.register(null, "12345"));
+//    }
+//
+//    @Test
+//    public void register_nullPassword() throws Exception {
+//        assertFalse(activity.register("jkurtz678@gmail.com", null));
+//    }
+//
+//    @Test
+//    public void register_badEmail() throws Exception {
+//        assertFalse(activity.register("jkurtz678", "12345"));
+//    }
+//
+//    // longer than 4, shorter than 16
+//    @Test
+//    public void register_badPasswordLength() throws Exception {
+//        assertTrue(activity.register("jkurtz678@gmail.com", "12345"));
+//        assertFalse(activity.register("jkurtz678@gmail.com", "123"));
+//        assertFalse(activity.register("jkurtz678@gmail.com", "123423123134235111"));
+//    }
+//
+//    @Test
+//    public void register_invalidCharacters() throws Exception {
+//        assertFalse(activity.register("jac##  kson kurtz@gmail.com", "12345"));
+//
+//    }
 }
