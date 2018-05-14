d2ca975508a304cf7d7e25e487c9625060598879
Eitan Simler <eitan.simler@gmail.com>
2018-05-13 22:56:15 -0700

    Added Playlist Tests

--- app/src/test/java/com/houseparty/houseparty/PlaylistUnitTest.java
+++ app/src/test/java/com/houseparty/houseparty/PlaylistUnitTest.java
@@ -3,6 +3,9 @@ package com.houseparty.houseparty;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PlaylistUnitTest {
@@ -15,22 +18,43 @@ public class PlaylistUnitTest {
     }
 
     @Test
-    public void test_toString_oneSong() {
+    public void testToStringOneSong() {
         playlist.addSong(new SpotifySong("Thriller", "fake_uri", "Michael Jackson"));
         assertTrue(playlist.toString().equals("JacksonThriller"));
     }
 
     @Test
-    public void test_toString_twoSongs() {
+    public void testToStringTwoSongs() {
         playlist.addSong(new SpotifySong("Thriller", "fake_uri", "Michael Jackson"));
         playlist.addSong(new SpotifySong("Beat It", "fake_uri2", "Michael Jackson"));
         assertTrue(playlist.toString().equals("JacksonThriller"));
     }
 
     @Test
-    public void test_toString_twoSongs2() {
+    public void testToStringTwoSongs2() {
         playlist.addSong(new SpotifySong("Beat It", "fake_uri2", "Michael Jackson"));
         playlist.addSong(new SpotifySong("Thriller", "fake_uri", "Michael Jackson"));
         assertTrue(playlist.toString().equals("JacksonBeat It"));
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals(playlist.getName(), "Jackson");
+    }
+
+    @Test
+    public void testGetPasscode() {
+        assertEquals(playlist.getPasscode(), "12345");
+    }
+
+    @Test
+    public void testGetSongs() {
+        assertEquals(playlist.getSongs(), new ArrayList<Song>());
+    }
+
+    @Test
+    public void testNoArgumentConstructor() {
+        Playlist playlist = new Playlist();
+        assertEquals(playlist.getName(), null);
     }
 }
