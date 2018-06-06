76c3b5d8e9ecba2fc67342497e66e046a61774ba
Eitan Simler <eitan.simler@gmail.com>
2018-06-06 01:35:00 -0700

    Some GUI tests

--- app/src/androidTest/java/com/houseparty/houseparty/LoginTextTest.java
+++ app/src/androidTest/java/com/houseparty/houseparty/LoginTextTest.java
@@ -1,10 +1,17 @@
 package com.houseparty.houseparty;
 
-//import android.support.test.rule.ActivityTestRule;
-
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * @author Eitan created on 6/1/2018.
@@ -12,9 +19,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class LoginTextTest {
-    private String emailToBeTyped;
-    private String passwordToBeTyped;
+    @Rule
+    public ActivityTestRule<LoginActivity> loginActivityActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
-//    @Rule
-//    public ActivityTestRule<LoginActivity> loginActivityActivityTestRule = new ActivityTestRule<>(LoginActivity.class)
+    @Test
+    public void validateEmailAddress() {
+        onView(withId(R.id.editText)).perform(typeText("example@email.com")).check(matches(withText("example@email.com")));
+    }
+
+    @Test
+    public void validatePassword() {
+        onView(withId(R.id.editText2)).perform(typeText("password")).check(matches(withText("password")));
+    }
 }
